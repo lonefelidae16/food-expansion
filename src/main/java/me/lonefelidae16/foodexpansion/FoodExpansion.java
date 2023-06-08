83a693389a56aec462aedaaf3b28f215bed56636
lonefelidae16 <kow161206@gmail.com>
2023-06-09 08:39:09 +0900

    [core] Port to MC1.20
    * update `gradle-wrapper.properties`
      - Gradle 8.1.1
    
    * update `build.gradle`
      - fabric loom 1.2-SNAPSHOT
      - artifactory 4.31.5
      - add external maven repo
    
    * update `gradle.properties`
      - yarn mappings 1.20+build.1
      - fabric api 0.83.0
      - fabric loader 0.14.21
    
    * update `fabric.mod.json`
      - fabricloader *any*
      - minecraft ~1.20

--- src/main/java/me/lonefelidae16/foodexpansion/FoodExpansion.java
+++ src/main/java/me/lonefelidae16/foodexpansion/FoodExpansion.java
@@ -1,16 +1,16 @@
 package me.lonefelidae16.foodexpansion;
 
+import com.mojang.logging.LogUtils;
 import me.lonefelidae16.foodexpansion.items.ItemsInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FoodExpansion implements ModInitializer {
     public static final String MOD_ID = "foodexpansion";
-    public static final Logger LOGGER = LoggerFactory.getLogger(FoodExpansion.MOD_ID);
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     @Contract(value = "_ -> new", pure = true)
     public static @NotNull Identifier ID(@NotNull String path) {
--- src/main/java/me/lonefelidae16/foodexpansion/compat/extrasounds/ExtraSoundsGenerator.java
+++ src/main/java/me/lonefelidae16/foodexpansion/compat/extrasounds/ExtraSoundsGenerator.java
@@ -1,5 +1,6 @@
 package me.lonefelidae16.foodexpansion.compat.extrasounds;
 
+import dev.stashy.extrasounds.ExtraSounds;
 import dev.stashy.extrasounds.mapping.SoundDefinition;
 import dev.stashy.extrasounds.mapping.SoundGenerator;
 import dev.stashy.extrasounds.sounds.Categories;
@@ -14,17 +15,17 @@ import net.minecraft.item.StewItem;
 import net.minecraft.util.Identifier;
 
 public class ExtraSoundsGenerator {
-    public static SoundGenerator SOUND_GENERATOR = SoundGenerator.of(FoodExpansion.MOD_ID, FoodExpansion.MOD_ID, item -> {
+    public static SoundGenerator SOUND_GENERATOR = SoundGenerator.of(FoodExpansion.MOD_ID, item -> {
         if (item instanceof BreadItem) {
-            return SoundDefinition.of(Sounds.event(new Identifier("extrasounds", "item.pickup.minecraft.bread"), 1.f));
+            return SoundDefinition.of(Sounds.event(new Identifier(ExtraSounds.MODID, "item.pickup.minecraft.bread"), 1.f));
         } else if (item instanceof PieItem) {
             return SoundDefinition.of(Sounds.aliased(Categories.WET));
         } else if (item instanceof CreamItem) {
             return SoundDefinition.of(Sounds.aliased(Categories.WET));
         } else if (item instanceof StewItem) {
-            return SoundDefinition.of(Sounds.event(new Identifier("extrasounds", "item.pickup.minecraft.rabbit_stew"), 1.f));
+            return SoundDefinition.of(Sounds.event(new Identifier(ExtraSounds.MODID, "item.pickup.minecraft.rabbit_stew"), 1.f));
         } else if (item == ItemsInitializer.BAKED_APPLE) {
-            return SoundDefinition.of(Sounds.event(new Identifier("extrasounds", "item.pickup.minecraft.apple"), 1.f));
+            return SoundDefinition.of(Sounds.event(new Identifier(ExtraSounds.MODID, "item.pickup.minecraft.apple"), 1.f));
         } else if (item == ItemsInitializer.BUCKET_CARAMEL_CUSTARD) {
             return SoundDefinition.of(Sounds.single(new Identifier("block.mud.step"), .45f, 1.9f, Sound.RegistrationType.SOUND_EVENT));
         }
